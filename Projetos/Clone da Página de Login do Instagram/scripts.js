d7328443758eeac4b23f950157ce11191d0bdbd7
José Gabriel de Andrade Almeida <zegabrielandrade@gmail.com>
2025-06-02 13:45:58 -0300

    Add files via upload

---
+++ Projetos/Clone da Página de Login do Instagram/scripts.js
@@ -0,0 +1,18 @@
+let imagem = document.getElementById("troca-imagem")
+
+
+function trocaImagem() {
+    if (imagem.style.opacity == 0) {
+        imagem.style.opacity = 1
+    } else {
+        imagem.style.opacity = 0
+    }
+}
+
+setInterval(() => {
+    trocaImagem()
+}, 3000)
+
+// 3000 milisegundos = 3 segundos
+// 1 = Colocando um Valor
+// 2 == Comparando/Comparação
